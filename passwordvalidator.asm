06245868156daf058902a496b80223881e6a854c
Pablo <69274746+pablousky4@users.noreply.github.com>
2026-04-07 20:48:25 +0200

    Añadir imput contraseña
    Agregamos la funcion para que el usuario pueda introducir su contraseña por teclado

--- passwordvalidator.asm
+++ passwordvalidator.asm
@@ -1,17 +1,35 @@
 %include "io.inc"
 
 extern printf
+extern scanf
 
 section .data
     password db "1234", 0
     mensaje db "Sistema iniciado", 10, 0
+    pedir db "Introduce la contraseña: ", 0
+    formato db "%s", 0
+
+section .bss
+    input resb 20   ; espacio para 20 caracteres
 
 section .text
 global main
 
 main:
+    ; Mostrar mensaje inicial
     push mensaje
     call printf
     add esp, 4
+
+    ; Pedir contraseña
+    push pedir
+    call printf
+    add esp, 4
+
+    ; Leer input del usuario
+    push input
+    push formato
+    call scanf
+    add esp, 8
 
     ret
